735d704ba08ac8289b5773273b4c2c0a6f3879b7
Wenbo-文博 <103347797+Stringzwb@users.noreply.github.com>
2023-09-27 16:34:58 +0800

    Update clientTest.java

--- PrototypeMethod/clientTest.java
+++ PrototypeMethod/clientTest.java
@@ -9,10 +9,10 @@ public class clientTest {
     public static void main(String[] args) {
         // 初始化原始对象
         List<String> menuItems = new ArrayList<>();
-        menuItems.add("a");
-        menuItems.add("b");
-        menuItems.add("c");
-        ConcretePrototypeMenus menus = new ConcretePrototypeMenus("a", menuItems);
+        menuItems.add("牛奶");
+        menuItems.add("咖啡");
+        menuItems.add("茶");
+        ConcretePrototypeMenus menus = new ConcretePrototypeMenus("菜单A", menuItems);
 
         // 创建浅拷贝和深拷贝对象
         ConcretePrototypeMenus menus1 = menus.shallowClone();
@@ -25,10 +25,10 @@ public class clientTest {
         System.out.println("Deep Clone: " + menus2);
 
         // 修改原始对象、浅拷贝和深拷贝对象
-        menuItems.set(0, "Modified");
-        menus1.setDate("Modified Date");
+        menuItems.set(0, "果汁");
+        menus1.setDate("菜单B");
         List<String> deepCloneItems = menus2.getItems();
-        deepCloneItems.set(0, "Deep Clone Modified");
+        deepCloneItems.set(0, "面包");
 
         // 输出修改后的对象，检查如何影响其他对象
         System.out.println("\nAfter modification:");
